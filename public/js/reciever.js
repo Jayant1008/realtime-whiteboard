65da1239804efef7931a9966a69dcfcf1a755e35
Jayant <148469367+Jayant1008@users.noreply.github.com>
2025-08-20 06:35:17 +0530

    made realtime

---
+++ public/js/reciever.js
@@ -0,0 +1,36 @@
+const socket = io();
+socket.on("onpensize", function(size) {
+  penSize(size);
+});
+socket.on("onerasersize", function(size) {
+  eraserSize(size);
+});
+socket.on("oncolor", function(color) {
+  colorChanger(color);
+});
+socket.on("ontoolchange", function(tool) {
+  handleTool(tool);
+});
+
+socket.on("onmousedown", function(point) {
+  const { x, y, color, width } = point;
+  canvas.lineWidth = width;
+  canvas.strokeStyle = color;
+  canvas.beginPath();
+  canvas.moveTo(x, y);
+  undoStack.push(point);
+});
+socket.on("onmousemove", function(point) {
+  const { x, y, color, width } = point;
+  canvas.lineWidth = width;
+  canvas.strokeStyle = color;
+  canvas.lineTo(x, y);
+  canvas.stroke();
+  undoStack.push(point);
+});
+socket.on("onundo", function() {
+  undoMaker();
+});
+socket.on("onredo", function() {
+  redoMaker();
+});
---
+++ public/js/undoredo.js
@@ -0,0 +1,49 @@
+let undoStack = [];
+let redoStack = [];
+function undoMaker() {
+  if (undoStack.length > 0) {
+    redoStack.push(undoStack.pop());
+    redraw();
+    return true;
+  }
+  return false;
+}
+function redoMaker() {
+  if (redoStack.length > 0) {
+    undoStack.push(redoStack.pop());
+    redraw();
+    return true;
+  }
+  return false;
+}
+
+let undo = document.querySelector("#undo");
+let redo = document.querySelector("#redo");
+let interval;
+undo.addEventListener("mousedown", function() {
+  interval = setInterval(function() {
+    if (undoMaker()) socket.emit("undo");
+  }, 50);
+});
+
+undo.addEventListener("mouseup", function () {
+  clearInterval(interval); 
+});
+
+undo.addEventListener("mouseleave", function () {
+  clearInterval(interval);
+});
+
+redo.addEventListener("mousedown", function() {
+  interval = setInterval(function() {
+    if (redoMaker()) socket.emit("redo");
+  }, 50);
+});
+
+redo.addEventListener("mouseup", function () {
+  clearInterval(interval); 
+});
+
+redo.addEventListener("mouseleave", function () {
+  clearInterval(interval);
+});
